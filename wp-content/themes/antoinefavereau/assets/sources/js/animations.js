659931c0cb2b47eab3b505a88befa3467c4a2624
Antoine FAVEREAU <antoine.favereau@viacesi.fr>
2023-09-08 12:32:12 +0200

    🚧 WIP animations

--- wp-content/themes/antoinefavereau/assets/sources/js/animations.js
+++ wp-content/themes/antoinefavereau/assets/sources/js/animations.js
@@ -1,4 +1,22 @@
+// gsap.registerPlugin(ScrollTrigger);
+
 var topSectionTL = gsap.timeline();
 
-topSectionTL.from(".topSectionUpElasticAnimation", {duration: 3, y: 500, ease: "elastic.out(1, 0.6)", stagger: 0.2});
+topSectionTL.from(".topSectionUpElasticAnimation", {
+    duration: 3,
+    y: 500,
+    opacity: 0,
+    ease: "elastic.out(1, 0.6)",
+    stagger: 0.2
+});
 
+// gsap.from("#background .parcoursLIst .item", {
+//     scrollTrigger: {
+//         trigger: "#background .parcoursLIst",
+//         toggleActions: "repeat none none none"
+//     },
+//     duration: 1,
+//     y: 200,
+//     opacity: 0,
+//     stagger: 0.2
+// });
