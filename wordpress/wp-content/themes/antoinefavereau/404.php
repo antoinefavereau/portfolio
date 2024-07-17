07efe95616a8a50e9f908cfdd930e0fad310cf4e
Antoine Favereau <antoinefavereau45@gmail.com>
2024-07-17 18:21:34 +0200

    404 page

--- wordpress/wp-content/themes/antoinefavereau/404.php
+++ wordpress/wp-content/themes/antoinefavereau/404.php
@@ -2,6 +2,11 @@
 get_header();
 ?>
 
-<h1>404</h1>
-<p>page not found</p>
-<a href="<?= get_home_url() ?>" class="button">home page</a>
+<div class="errorPage">
+    <h1>404</h1>
+    <p>page not found</p>
+    <a href="<?= get_home_url() ?>" class="button">home page</a>
+</div>
+
+<?php
+get_footer();
