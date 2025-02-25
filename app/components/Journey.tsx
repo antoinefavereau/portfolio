1604bd099861f42b67cec9ebf78b05faf78a08be
Antoine Favereau <93597506+antoinefavereau@users.noreply.github.com>
2025-02-25 18:28:28 +0000

    wip journey section

--- app/components/Journey.tsx
+++ app/components/Journey.tsx
@@ -19,14 +19,21 @@ export default function Journey({ texts, journey }: JourneyProps) {
         </h2>
         <p className="text-4xl font-medium">{texts.subtitle}</p>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2">
+        {/* <3 */}
         {journey.map((item, index) => (
           <div key={index} className="flex">
-            <div className="flex flex-col items-end gap-2">
+            <div className="w-1/4 flex flex-col items-end gap-2 shrink-0">
               <h3 className="text-2xl font-bold">{item.title}</h3>
               <p className="text-lg">{item.dates}</p>
             </div>
-            <p>{item.description}</p>
+            <div className="flex flex-col items-center gap-2 px-8">
+              <span className="w-5 h-5 rounded-full bg-primary"></span>
+              <span className="w-1 grow bg-primary"></span>
+            </div>
+            <div>
+              <p className="text-start">{item.description}</p>
+              </div>
           </div>
         ))}
       </div>
