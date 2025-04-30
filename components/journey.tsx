78c572f144dab0a98fac50932f5eb2a0d75ad8d2
Antoine Favereau <93597506+antoinefavereau@users.noreply.github.com>
2025-04-30 18:12:21 +0000

    feat: journey section + dark background

--- components/journey.tsx
+++ components/journey.tsx
@@ -12,28 +12,25 @@ interface JourneyProps {
 
 export default function Journey({ texts, journey }: JourneyProps) {
   return (
-    <section className="bg-white text-black flex flex-col justify-center items-center gap-12 text-center py-16 px-2">
-      <div className="max-w-2xl flex flex-col items-center gap-4">
+    <section className="bg-white text-black flex flex-col justify-center items-center gap-24 py-16 px-8 md:px-16">
+      <div className="max-w-xl flex flex-col items-center gap-4 text-center">
         <h2 className="text-primary text-2xl font-medium uppercase">
           {texts.title}
         </h2>
         <p className="text-4xl font-medium">{texts.subtitle}</p>
       </div>
       <div className="flex flex-col gap-2">
-        {/* <3 */}
         {journey.map((item, index) => (
-          <div key={index} className="flex">
+          <div key={index} className="group flex gap-8">
             <div className="w-1/4 flex flex-col items-end gap-2 shrink-0">
-              <h3 className="text-2xl font-bold">{item.title}</h3>
+              <h3 className="text-3xl font-bold text-end">{item.title}</h3>
               <p className="text-lg">{item.dates}</p>
             </div>
             <div className="flex flex-col items-center gap-2 px-8">
               <span className="w-5 h-5 rounded-full bg-primary"></span>
-              <span className="w-1 grow bg-primary"></span>
+              <span className="w-1 grow bg-primary group-last:hidden"></span>
             </div>
-            <div>
-              <p className="text-start">{item.description}</p>
-              </div>
+            <p className="max-w-xl mb-16 group-last:mb-0">{item.description}</p>
           </div>
         ))}
       </div>
