c7538ef1a4dbdbd9ae3b5e7d4daefecbfe565796
AzureAD\AntoineFavereau <antoine.favereau@evermaps.com>
2025-06-09 18:36:19 +0200

    responsive journey section

--- components/journey.tsx
+++ components/journey.tsx
@@ -18,16 +18,24 @@ export default function Journey({ texts, journey }: JourneyProps) {
       <SectionHeader title={texts.title} subtitle={texts.subtitle} />
       <div className="flex flex-col gap-2">
         {journey.map((item, index) => (
-          <div key={index} className="group flex gap-8">
-            <div className="w-1/4 flex flex-col items-end gap-2 shrink-0">
+          <div key={index} className="group flex gap-[5%]">
+            <div className="w-1/4 flex-col items-end gap-2 shrink-0 hidden md:flex -mt-3">
               <h3 className="text-3xl font-bold text-end">{item.title}</h3>
               <p className="text-lg">{item.dates}</p>
             </div>
-            <div className="flex flex-col items-center gap-2 px-8">
+            <div className="flex flex-col items-center gap-2">
               <span className="w-5 h-5 rounded-full bg-primary"></span>
               <span className="w-1 grow bg-primary group-last:hidden"></span>
             </div>
-            <p className="max-w-xl mb-16 group-last:mb-0">{item.description}</p>
+            <div className="flex flex-col gap-2 -mt-2">
+              <div className="flex flex-col gap-2 md:hidden">
+                <h3 className="text-2xl font-bold">{item.title}</h3>
+                <p className="text-lg">{item.dates}</p>
+              </div>
+              <p className="max-w-xl mb-8 md:mb-16 group-last:mb-0">
+                {item.description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
