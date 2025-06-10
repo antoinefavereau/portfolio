619e907866221213f6b56e5cdfbaa570c1844734
AzureAD\AntoineFavereau <antoine.favereau@evermaps.com>
2025-06-10 09:45:30 +0200

    fix build

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({ params }: PageProps) {
         <Skills texts={texts.skills_section} skills={skills} />
         <Projects texts={texts.projects_section} projects={projects} />
       </main>
-      <Footer texts={texts.footer_section} />
+      <Footer />
     </>
   );
 }
