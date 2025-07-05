7feb243df436b6041d87ddede1785b509d4ca29f
AzureAD\AntoineFavereau <antoine.favereau@evermaps.com>
2025-07-05 12:41:22 +0200

    refactor: update journey component layout for improved responsiveness

--- components/journey.tsx
+++ components/journey.tsx
@@ -16,7 +16,7 @@ export default function Journey({ texts, journey }: JourneyProps) {
   return (
     <section className="bg-white text-black flex flex-col justify-center items-center gap-24 py-16 px-8 md:px-16">
       <SectionHeader title={texts.title} subtitle={texts.subtitle} />
-      <div className="flex flex-col gap-2">
+      <div className="grid gap-2 w-full max-w-6xl">
         {journey.map((item, index) => (
           <div key={index} className="group flex gap-[5%]">
             <div className="w-1/4 flex-col items-end gap-2 shrink-0 hidden md:flex -mt-3">
@@ -32,7 +32,7 @@ export default function Journey({ texts, journey }: JourneyProps) {
                 <h3 className="text-2xl font-bold">{item.title}</h3>
                 <p className="text-lg">{item.dates}</p>
               </div>
-              <p className="max-w-xl mb-8 md:mb-16 group-last:mb-0">
+              <p className="mb-8 md:mb-16 group-last:mb-0">
                 {item.description}
               </p>
             </div>
