4b148e972c5fe742f6af25e64490a51820eff688
AzureAD\AntoineFavereau <antoine.favereau@evermaps.com>
2025-07-10 10:29:04 +0200

    improve projetcs hover + clsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -16,16 +16,18 @@ interface ProjectsProps {
 
 export default function Projects({ texts, projects }: ProjectsProps) {
   return (
-    <section className="bg-white text-black flex flex-col justify-center items-center gap-24 py-16 px-8 md:px-16">
-      <SectionHeader title={texts.title} subtitle={texts.subtitle} />
-      <div className="flex flex-col w-full px-16">
+    <section className="bg-white text-black flex flex-col justify-center items-center gap-24 py-16">
+      <SectionHeader
+        className="px-8 md:px-16"
+        title={texts.title}
+        subtitle={texts.subtitle}
+      />
+      <div className="grid w-full">
         {projects.map((item, index) => (
-          <div
-            key={index}
-            className="group w-full grid grid-cols-[auto_3fr_4fr] gap-8 border-b-1 border-foreground py-16"
-          >
-            <span>{String(index + 1).padStart(2, "0")}</span>
-            <div className="relative">
+          <div className="group grid px-24 md:px-32" key={index}>
+            <div className="grid grid-cols-[auto_3fr_4fr] gap-8 border-b-1 border-foreground py-16">
+              <span>{String(index + 1).padStart(2, "0")}</span>
+              <div className="relative">
                 <Image
                   className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full h-auto aspect-square scale-0 group-hover:scale-100 transition-all duration-300 object-contain object-center"
                   src={"/projects/" + item.image}
@@ -33,13 +35,14 @@ export default function Projects({ texts, projects }: ProjectsProps) {
                   width={500}
                   height={300}
                 />
-            </div>
-            <div className="flex flex-col gap-4">
-              <h3 className="text-4xl font-bold uppercase">
-                {item.title}
-                <span className="inline-block h-[6px] w-6 bg-primary ml-2"></span>
-              </h3>
-              <p className="">{item.description}</p>
+              </div>
+              <div className="flex flex-col gap-4">
+                <h3 className="text-4xl font-bold uppercase">
+                  {item.title}
+                  <span className="inline-block h-[6px] w-6 bg-primary ml-2"></span>
+                </h3>
+                <p className="">{item.description}</p>
+              </div>
             </div>
           </div>
         ))}
--- components/ui/section-header.tsx
+++ components/ui/section-header.tsx
@@ -1,12 +1,23 @@
+import clsx from "clsx";
+
 export default function SectionHeader({
   title,
   subtitle,
+  className,
+  ...props
 }: {
   title: string;
   subtitle: string;
-}) {
+  className?: string;
+} & React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="max-w-xl flex flex-col items-center gap-4 text-center">
+    <div
+      className={clsx(
+        "max-w-xl flex flex-col items-center gap-4 text-center",
+        className
+      )}
+      {...props}
+    >
       <h2 className="text-primary text-2xl font-medium uppercase">{title}</h2>
       <p className="text-4xl font-medium">{subtitle}</p>
     </div>
